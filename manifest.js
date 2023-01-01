4d655b0b976f2eafd37df8573a9098c5ebe3efb9
Apollo Wayne <waynezhaoyang@gmail.com>
2023-01-01 10:30:45 +0800

    Set Version

--- manifest.js
+++ manifest.js
@@ -1,6 +1,7 @@
 import fs from "fs";
 import path from "path";
 import rcedit from "rcedit";
+import { version } from "./package.json";
 
 const getFiles = (folder, filter) => {
     return fs.readdirSync(folder).filter(item => filter.test(item)).map(item => path.join(folder, item));
@@ -8,6 +9,9 @@ const getFiles = (folder, filter) => {
 
 for(const file of getFiles("./bin", /\.exe$/i)){
   await rcedit(file, {
+    "version-string": version,
+    "file-version": version,
+    "product-version": version,
     icon: "./app.ico"
   });
 }
