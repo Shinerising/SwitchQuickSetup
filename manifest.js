017fd5629837c9a36978afbf81bd1063f6a06b74
Apollo Wayne <waynezhaoyang@gmail.com>
2023-01-01 10:00:57 +0800

    Add ARM64 Build

--- manifest.js
+++ manifest.js
@@ -1,5 +1,13 @@
-import rcedit from 'rcedit';
+import fs from "fs";
+import path from "path";
+import rcedit from "rcedit";
 
-await rcedit("./bin/switchsetup.exe", {
-  icon: "./app.ico"
-});
+const getFiles = (folder, filter) => {
+    return fs.readdirSync(folder).filter(item => filter.test(item)).map(item => path.join(folder, item));
+};
+
+for(const file of getFiles("./bin", /\.exe$/i)){
+  await rcedit(file, {
+    icon: "./app.ico"
+  });
+}
