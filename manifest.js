461fae2d0221f99db8301b3c372016cdd7662b51
Apollo Wayne <waynezhaoyang@gmail.com>
2023-01-01 10:35:15 +0800

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,7 +1,6 @@
 import fs from "fs";
 import path from "path";
 import rcedit from "rcedit";
-import { version } from "./package.json";
 
 const getFiles = (folder, filter) => {
     return fs.readdirSync(folder).filter(item => filter.test(item)).map(item => path.join(folder, item));
@@ -9,9 +8,9 @@ const getFiles = (folder, filter) => {
 
 for(const file of getFiles("./bin", /\.exe$/i)){
   await rcedit(file, {
-    "version-string": version,
-    "file-version": version,
-    "product-version": version,
+    "version-string": process.env.npm_package_version,
+    "file-version": process.env.npm_package_version,
+    "product-version": process.env.npm_package_version,
     icon: "./app.ico"
   });
 }
